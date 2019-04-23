3e83eea20d948348cc37b905105e5149210b1b5d
senli <senli@lagou.com>
2019-04-23 16:01:03 +0800

    init

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
 
 export default {
     input: 'lib/index.js',
     output: {
         file: 'dist/index.js',
         format: 'umd'
-    },
-    plugins: [
-        resolve(),
-        babel({
-            exclude: 'node_modules/**'
-        })
-    ]
+    }
 };
